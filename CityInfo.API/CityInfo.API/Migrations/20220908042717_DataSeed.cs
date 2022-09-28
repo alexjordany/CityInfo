aaaa144c93fafe21f2f9828c61e5180e8d6a72cc
Alex Jordany <alexjordany@outlook.com>
2022-09-27 20:46:10 -0600

    Base solution

---
+++ CityInfo.API/CityInfo.API/Migrations/20220908042717_DataSeed.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "PointOfInterests",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "PointOfInterests",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
